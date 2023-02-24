fb51e9669bde65874b96639605fd78a6d3b4afec
JotaPeans <jpfontesferreira1@gmail.com>
2023-02-23 21:22:44 -0300

    update

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 import { resolve } from "path";
 
 config({
-    path: resolve(__dirname, "../.env")
+    path: resolve(__dirname, "../../.env")
 })
 
 async function startDB() {
